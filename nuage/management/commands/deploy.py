d9ed0f2d765fa74bd20498915e919b5b7f94d510
Jorge Eduardo Cardona <jorgeecardona@gmail.com>
2010-12-02 15:44:16 -0500

    Ask for variables if they didn't appear at the settings

--- nuage/management/commands/deploy.py
+++ nuage/management/commands/deploy.py
@@ -4,6 +4,7 @@ import tarfile
 import base64
 import urllib
 import urllib2
+from getpass import getpass
 from django.core.management.base import BaseCommand
 # from optparse import make_option
 from django.conf import settings
@@ -16,21 +17,41 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print "Start deploying."
 
-        # Name of application.
-        application = settings.NUAGE_APPLICATION
-        email = settings.NUAGE_EMAIL
-        key = settings.NUAGE_KEY
-        version = settings.NUAGE_VERSION
+        # User's email
+        if hasattr(settings, 'NUAGE_EMAIL'):
+            email = settings.NUAGE_EMAIL
+        else:
+            email = raw_input("Please enter your email: ")
+
+        # User's deployment key
+        if hasattr(settings, 'NUAGE_KEY'):
+            key = settings.NUAGE_KEY
+        else:
+            key = getpass("Please enter your deployment key: "))
+
+        # Application's name
+        if hasattr(settings, 'NUAGE_APPLICATION'):
+            application = settings.NUAGE_APPLICATION
+        else:
+            application = raw_input("Please enter the application's name: ")
+
+        # application's version
+        if hasattr(settings, 'NUAGE_VERSION'):
+            version = settings.NUAGE_VERSION
+        else:
+            version = raw_input("Please enter the application's version: ")
 
         # Prepare
-        print "Prepare application %s." % (application, )
+        print "Preparing to deploy in %(version)s.%(application)s.apps.cenuage.com ..." % {
+            'version': version,
+            'application': application,
+            })
 
         # Import settings.
         settings_module = __import__(settings.SETTINGS_MODULE)
         dirname = os.path.dirname(settings_module.__file__)
 
         # Start compressing
-        print "Create compress file with %s" % (os.path.abspath(dirname), )
         tempfd = tempfile.NamedTemporaryFile(suffix='.tar.gz')
 
         # Create tar.
